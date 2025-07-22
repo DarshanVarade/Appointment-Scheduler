8d4b68a469c6704db831e439c416760ce855a054
Darshan Varade <132351325+DarshanVarade@users.noreply.github.com>
2025-07-22 23:06:28 +0530

    update some files

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
     );
     console.log("MongoDB connection Successful");
   } catch (error) {
