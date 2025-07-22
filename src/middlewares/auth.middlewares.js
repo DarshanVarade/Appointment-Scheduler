b3a11cf469ccf754c28a7469095a33bf04598ae9
Darshan Varade <132351325+DarshanVarade@users.noreply.github.com>
2025-07-22 22:55:28 +0530

    romove errors

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.models";
+import { User } from "../models/user.models.js";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/ApiError";
 
