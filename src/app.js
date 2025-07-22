3cc9b31d7e04039d803a483d3e76afbc924a7f2c
Darshan Varade <132351325+DarshanVarade@users.noreply.github.com>
2025-07-22 11:24:22 +0530

    add basic files

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CROS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//import routers
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import { errorHandler } from "./middlewares/error.middlewares.js";
+
+//routers
+app.use("/api/v1/healthcheck", healthcheckRouter);
+
+app.use(errorHandler);
+export { app };
